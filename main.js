dba36bd768851ad2df60d7c098bc41b40234fc86
gangee3 <gangee135@gmail.com>
2021-01-28 15:42:01 +0900

    try catch 사용

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ const app = express();
 const port = 3000;
 const db = require('./lib/db');
 const bodyParser = require('body-parser');
+const { indexOf } = require('methods');
+const { error } = require('console');
 const router = express.Router();
 
 app.use(express.json()); 
@@ -20,110 +22,120 @@ router.route('/signUp').post((req, res) => {
     let paramMajor = req.body.major;
     let paramPosition = req.body.position;
     let paramKakaoId = req.body.kakaoId;
-    signUp(paramName, paramStudentId, paramMajor, paramPosition, paramKakaoId, function(err){
-        if(err){
-            console.log(err);
-            if(err.indexOf('1062')){
-                res.send(-101);
-                //학번 또는 카카오 아이디가 겹침
+    try{
+        signUp(paramName, paramStudentId, paramMajor, paramPosition, paramKakaoId, (err, results) => {
+            try{
+                res.send(100);
+                return;
             }
-        }
-        else{
-            res.send(100);
-
-        }
-    });
+            catch{
+                if(err.indexOf('1062')){
+                    res.send(-101);
+                    return;
+                }
+            }
+        });
     }
-);
+    catch{
+        console.log(error);
+    }
+});
 
 router.route('/signIn').post((req, res) => {
-    let paramKakaoid = req.body.kakaoid;
-    signIn(paramKakaoid, function(err, results){
-        if(err){
-            console.log(err);
-            res.send(-201);//카카오 아이디가 존재하지 않음
-        }
-        else{
-            res.send(200);
-            res.json({
-                name : results[0],
-                studentId : results[1],
-                major : results[2],
-                position : results[3]
-            });
-        }
-    });
+    let paramKakaoId = req.body.kakaoId;
+    try{
+        signIn(paramKakaoId, (err, results) =>{
+            try{
+                res.send(200);
+                res.json({
+                    name : results[0].MEMBERNAME,
+                    studentId : results[0].STUDENTID,
+                    major : results[0].MAJOR,
+                    position : results[0].POSITION
+                });
+                return;
+            }
+            catch{
+                res.send(-201);
+                return;
+            }
+        });
     }
-);
+    finally{
+        return;
+    }
+});
 
-var signUp = function(name, studentId, major, position, kakaoId, callback){
-    db.getConnection(function(err, connection){
-        if(err){
-            if(connection){
-                connection.release();
-                
+var signUp = (name, studentId, major, position, kakaoId, callback) => {
+    try{
+        db.getConnection((err,connection) => {
+            try{
+                connection.query(`INSERT INTO MEMBER (MEMBERNAME, STUDENTID, MAJOR, POSITION, KAKAOID) VALUES(?, ?, ?, ?, ?)`, [name, studentId, major, position, kakaoId], (err,results) => {
+                    try{
+                        callback(null, results);
+                        return;        
+                    }
+                    finally{
+                        connection.release();
+                        return;
+                    }
+                });
             }
-            callback(err, null);
+            catch{
+                callback(err, null);
+                connection.release();
+                return;
+            }
+        });
+    }
+    catch{
+        if(connection){
+            connection.release();
             return;
         }
-        else{
-            connection.query(`
-            INSERT INTO Member (MEMBERNAME, STUDENTID, MAJOR, POSITION, KAKAOID) VALUES(?, ?, ?, ?, ?)`, [name, studentId, major, position, kakaoId], function(err, results){
-                connection.release();
-
-                if(err){
-                    callback(err, null);
-                    return;
-                }
-                else{
-                    callback(null, results);
-                    return;
-                }
-            });
-        }
-    });
-    
+        callback(err, null);
+        return;
+    }
 }
 
-var signIn = function(kakaoId, callback){
-    db.getConnection(function(err,connection){
-        if(err){
-            if(connection){
-                connection.release();
+var signIn = (kakaoId, callback) => {
+    try{
+        db.getConnection((err,connection) => {
+            try{
+                connection.query(`SELECT KAKAOID FROM Member WHERE KAKAOID = ?`, [kakaoId], (err, results) => {
+                    if(results.indexOf('Empty')){
+                        throw error;
+                    }
+                    try{
+                        connection.query(`SELECT MEMBERNAME, STUDENTID, MAJOR, POSITION FROM Member WHERE KAKAOID = ?`, [kakaoId], (err, rows, fields) => {
+                            try{
+                                callback(null, results);
+                                return;
+                            }
+                            finally{
+                                connection.release();
+                                return;
+                            }
+                        });
+                    }
+                    catch{
+                        callback(err, null);
+                        return;
+                    }
+                });
             }
-            callback(err, null);
-            return;
-        }
-        else{
-            connection.query(`SELECT KAKAOID FROM Member WHERE KAKAOID = ?`, [kakaoId], function(err, results){
+            catch{
                 connection.release();
-
-                if(results.indexOf('Empty')){
-                    callback(err, null);
-                    return;
-                }
-                else{
-                    connection.query(`SELECT MEMBERNAME, STUDENTID, MAJOR, POSITION FROM Member WHERE KAKAOID = ?`,[kakaoid], function(err, rows, fields){
-                        if(err){
-                            connection.release();
-                            console.log(err);
-                        }
-                        else{
-                            let results = [];
-                            pushResults(results);
-                            console.log('The solution is: ', results);
-                        }
-                    });
-                    callback(null, results);
-                    return;
-                }
-            }); 
+                callback(err, null);
+                return;
+            }
+        });
+    }
+    catch{
+        if(connection){
+            connection.release();
         }
-    })
-}
-
-var pushResults = function(rows){
-    for(var i = 0; i < results.length; i++){
-        results.push(rows[i].resultId);
+        callback(err, null);
+        return;
     }
 }
