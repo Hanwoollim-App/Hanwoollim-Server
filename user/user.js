c8e87a9608b78f694dc4f120594e0072279e821b
sung-hyun-21cse <shchohanyang0329@naver.com>
2021-07-17 00:27:43 +0900

    axios 모듈을 추가. 기존에 token없이 생각했던 잘못된 코드들 대거 삭제. token을 받아오는 axios 코드를 설비할 예정.

--- user/user.js
+++ user/user.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const axios = require('axios');
+const kakao = require('./user_kakao');
 const router = express.Router();
 
 const pool = require('../db');
@@ -9,46 +11,26 @@ router.use('/home', home);
 
 router.route('/')
     .get((req, res, next) => {
-        // session이 defined라면 바로 '/user/home'으로 쏴줍니다 (로그인 유지)
-        if(req.session.kakaoid === undefined){
+        // token이 defined라면 바로 '/user/home'으로 쏴줍니다 (로그인 유지)
+        if(req.session.token === undefined){
             res.status(200);
         }
         else{
             res.redirect(302, '/user/home');
         }
-    })
-    .post((req, res, next) => {
-        let kakaoid = req.body.kakaoid;
-        
-        // DB에 등록된 kakaoid 인지 확인합니다
-        pool.getConnection((err, db) => {
-            if (err) throw err;
-            let sql = `SELECT * FROM MEMBER`;
+    });
 
-            db.query(sql, (err, rows) => {
-                if(err) throw err;
+// 1. RedirectURI 로직, axios를 이용해 token을 받아옴
+// 2. 관리자용, 유저용 RedirectUri는 별개임
+// 3. 카카오로그인, 동의 페이지(인가코드 받기)는 클라이언트에서 처리
 
-                rows.forEach(row => {
-                    if(row.KAKAOID === kakaoid){
-                        req.session.kakaoid = kakaoid;
-                        req.session.save(() => {
-                            res.redirect(200, '/user/home');
-                        });
-                    }
-                });
-        
-        // 등록되지 않은 kakaoid 였을 경우, 위의 foreach에서 session을 할당받지 못해
-        // 여전히 undefined라는 점을 이용해 signup 페이지로 보냅니다
-                if(req.session.kakaoid === undefined){
-                    router.use('/signup', signup);
-                    req.session.kakaoid = kakaoid;
-                    req.session.save(() => {
-                        res.redirect(302, '/user/signup');
-                    });
-                }
-            });
-            db.release();
-        });
+// 토큰 받기는 서버에서 처리
+
+router.route('/account/sign-in/kakao/callback')
+    .get((req, res, next) => {
+        const code = req.query.code;
+
+        res.send('hi');
     });
 
 module.exports = router;
