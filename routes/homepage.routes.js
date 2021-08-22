6c1cf34897b6c2f0164856e0865bd56b31f30e71
Jaemani <leejaeman0227@gmail.com>
2021-08-22 21:30:45 +0900

    setHeader 추가 시도

--- routes/homepage.routes.js
+++ routes/homepage.routes.js
@@ -3,10 +3,8 @@ const controller = require("../controllers/homepage.controller");
 
 module.exports = function(app) {
     app.use(function(req, res, next) {
-        res.header(
-            "Access-Control-Allow-Headers",
-            "x-access-token, Origin, Content-Type, Accept"
-        );
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH');
+        res.header("Access-Control-Allow-Headers", "x-access-token, Origin, Content-Type, Accept");
         next();
     });
 
